5de7867e28cd08df0ee3f3497fc2d88326828e33
Diego Quijada <diegoqui@udel.edu>
2025-03-17 17:55:14 -0400

    added

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,53 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    enum Holiday {
+        Christmas = "🎄",
+        Halloween = "🎃",
+        IndependenceDay = "🎆",
+        NewYear = "🎉",
+        Thanksgiving = "🦃",
+    }
+
+    const [holiday, setHoliday] = useState<Holiday>(Holiday.NewYear);
+
+    function nextAlphabet(holiday: Holiday): Holiday {
+        return {
+            [Holiday.Christmas]: Holiday.Halloween,
+            [Holiday.Halloween]: Holiday.IndependenceDay,
+            [Holiday.IndependenceDay]: Holiday.NewYear,
+            [Holiday.NewYear]: Holiday.Thanksgiving,
+            [Holiday.Thanksgiving]: Holiday.Christmas,
+        }[holiday];
+    }
+
+    function nextYear(holiday: Holiday): Holiday {
+        return {
+            [Holiday.NewYear]: Holiday.IndependenceDay,
+            [Holiday.IndependenceDay]: Holiday.Thanksgiving,
+            [Holiday.Thanksgiving]: Holiday.Halloween,
+            [Holiday.Halloween]: Holiday.Christmas,
+            [Holiday.Christmas]: Holiday.NewYear,
+        }[holiday];
+    }
+
+    return (
+        <div>
+            <p>Holiday: {holiday}</p>
+            <Button
+                onClick={() => {
+                    setHoliday(nextAlphabet(holiday));
+                }}
+            >
+                Advance by Alphabet
+            </Button>
+            <Button
+                onClick={() => {
+                    setHoliday(nextYear(holiday));
+                }}
+            >
+                Advance by Year
+            </Button>
+        </div>
+    );
 }
